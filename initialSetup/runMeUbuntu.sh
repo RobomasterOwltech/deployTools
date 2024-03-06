d12d40ea712d7be819961602182bf59d4690f6e0
fabianglz993 <a01634223@tec.mx>
2024-03-05 22:19:46 -0600

    made it easier for noobs

--- initialSetup/runMeUbuntu.sh
+++ initialSetup/runMeUbuntu.sh
@@ -3,4 +3,34 @@
 # TODO: 
 #   * Complete packages list
 
-sudo apt install screen 
+installDependencies(){
+    apps=( "containerd", "docker", "screen", "arm-none-eabi" )
+    
+    for app in "${apps[@]}"
+    do
+        if [ "$1" ~ "ubuntu" ]; then
+            sudo apt -y install "$app"
+        fi
+    done
+}
+
+echo "        ,o."
+echo "       (   )"
+echo "        ) ("
+echo "      ,'   `."
+echo "  ,-'/       \`-."
+echo " (   `-.___,-'   )"
+echo "  `-.         ,-'"
+echo "     /`,---.'\"
+echo "    / /     \ \"
+echo "   ( (       ) )"
+echo "    \ `-._,-' /"
+echo "      `-._,-'"
+echo " ===== Welcome to the team! ===== "
+echo "This repo will setup most of the things yo need to start developing."
+
+installDependencies "ubuntu";
+
+echo ""
+echo " ===== Generating ssh keys for your github ===== " 
+ssh-keygen -f ~/.ssh/git_"$(whoami)"_key -t ed25519 -N ""
