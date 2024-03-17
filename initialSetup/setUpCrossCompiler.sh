a4a50bcfd9395d33dd8ac87471a788c1e474f804
Jorge Pérez <42452493+JorgePerC@users.noreply.github.com>
2024-03-17 00:11:40 -0600

    script to configure cross compiler

---
+++ initialSetup/setUpCrossCompiler.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -e
+# ===== How to run =====
+# $1 -> path to the downloaded tar file
+if [ -z "$1" ]; then 
+    echo "You need an argument"
+fi 
+
+# ===== Explanations =====
+# What is a toolchain: https://web.eecs.umich.edu/~prabal/teaching/resources/eecs373/toolchain-notes.pdf
+# Guided video: https://www.youtube.com/watch?v=imUiQkO9YHM&t=329s
+
+if [[ "$(whoami)" != "root" ]]; then
+    echo "You are not root ( ͠° ͟ʖ ͡°)" 
+    exit 1
+fi
+# Make sure there are no versions installed
+if sudo apt remove gcc-arm-none-eabi ; then
+    echo "Deleted distributed packages"
+fi
+
+# Extract to /opt
+sudo tar -xf $1 -C /opt
+
+# Create symlinks from opt bins to /usr
+sudo mkdir -p /usr/local/bin/gcc-arm-none-eabi
+
+# Add /usr symlinks to path
+sudo ln -s /opt/gcc-arm-none-eabi*/bin/* /usr/local/bin/gcc-arm-none-eabi/
+
+# Add to Path
+    # Not needed, bc is is already in usr bin
+#export PATH="/usr/local/bin/gcc-arm-none-eabi/:$PATH"
+
