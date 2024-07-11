74c0c09768ee479cf128278f41a3d74d559153ef
root <root@MSI-de-Sofia>
2024-07-11 11:18:20 -0600

    Added an installation for Docker in case it doesn't exist, installed runc and containerd, and the chupon was commented out.

--- initialSetup/runMeUbuntu.sh
+++ initialSetup/runMeUbuntu.sh
@@ -4,30 +4,41 @@
 # * Complete packages list
 
 installDependencies(){
-    apps=( "containerd", "docker", "screen")
+    apps=( "containerd" "docker" "screen")
     
     for app in "${apps[@]}"
     do
-        if [ "$1" ~ "ubuntu" ]; then
-            sudo apt -y install "$app"
+        if [[ "$1" == *ubuntu* ]]; then
+    sudo apt -y install "$app"
         fi
     done
 }
 
-echo "        ,o."
-echo "       (   )"
-echo "        ) ("
-echo "      ,'   `."
-echo "  ,-'/       \`-."
-echo " (   `-.___,-'   )"
-echo "  `-.         ,-'"
-echo "     /`,---.'\"
-echo "    / /     \ \"
-echo "   ( (       ) )"
-echo "    \ `-._,-' /"
-echo "      `-._,-'"
+#echo "        ,o."
+#echo "       (   )"
+#echo "        ) ("
+#echo "      ,'   `."
+#echo "  ,-'/       \`-."
+#echo " (   `-.___,-'   )"
+#echo "  `-.         ,-'"
+#echo "     /`,---.'\"
+#echo "    / /     \ \"
+#echo "   ( (       ) )"
+#echo "    \ `-._,-' /"
+#echo "      `-._,-'"
 echo " ===== Welcome to the team! ===== "
 echo "This repo will setup most of the things yo need to start developing."
+
+# verify docker is install
+if ! snap list | grep -q docker; then
+    sudo apt-get install docker
+else
+    echo "Docker ya está instalado"
+fi
+
+sudo apt-get update   
+sudo apt-get --fix-missing install runc   
+sudo apt-get --fix-missing install containerd
 
 installDependencies "ubuntu";
 
