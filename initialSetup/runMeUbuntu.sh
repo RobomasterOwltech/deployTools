a5aa9f9bc7c24c760099af3d86d31745d05774dc
root <root@MSI-de-Sofia>
2024-07-12 16:52:35 -0600

    eficientizar las descargas de app. Intentar imprimir la figura con lang y lc_all pero no funciona. cambia a codificacion utf-8 pero borra se reescribe el archivo asi que se borra. se arregla usando un ctrl+z

--- initialSetup/runMeUbuntu.sh
+++ initialSetup/runMeUbuntu.sh
@@ -3,43 +3,41 @@
 # TODO: 
 # * Complete packages list
 
+# Verificar la codificación del archivo
+#file_encoding=$(file -bi "$0" | awk -F "=" '{print $2}')
+#iconv -f "$file_encoding" -t en_UTF-8 "$0" > "$0.tmp"
+#mv "$0.tmp" "$0"
+
+export LANG="en_US.UTF-8"
+export LC_ALL="en_US.UTF-8"
+
 installDependencies(){
-    apps=( "containerd" "docker" "screen")
-    
+    apps=( "containerd" "docker" "screen" "runc")
+    sudo apt update -y
+    sudo apt upgrade -y
     for app in "${apps[@]}"
     do
-        if [[ "$1" == *ubuntu* ]]; then
-    sudo apt -y install "$app"
+        if [[ "$1" == "ubuntu" ]]; then
+            sudo apt -y install "$app"
         fi
     done
 }
 
-#echo "        ,o."
-#echo "       (   )"
-#echo "        ) ("
-#echo "      ,'   `."
-#echo "  ,-'/       \`-."
-#echo " (   `-.___,-'   )"
-#echo "  `-.         ,-'"
-#echo "     /`,---.'\"
-#echo "    / /     \ \"
-#echo "   ( (       ) )"
-#echo "    \ `-._,-' /"
-#echo "      `-._,-'"
+echo "        ,o."
+echo "       (   )"
+echo "        ) ("
+echo "      ,'   `."
+echo "  ,-'/       \`-."
+echo " (   `-.___,-'   )"
+echo "  `-.         ,-'"
+echo "     /`,---.'\"
+echo "    / /     \ \"
+echo "   ( (       ) )"
+echo "    \ `-._,-' /"
+echo "      `-._,-'"
 
 echo " ===== Welcome to the team! ===== "
 echo "This repo will setup most of the things yo need to start developing."
-
-# verify docker is install
-if ! snap list | grep -q docker; then
-    sudo apt-get install docker
-else
-    echo "Docker ya está instalado"
-fi
-
-sudo apt-get update   
-sudo apt-get --fix-missing install runc   
-sudo apt-get --fix-missing install containerd
 
 installDependencies "ubuntu";
 
